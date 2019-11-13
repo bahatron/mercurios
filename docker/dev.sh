7a56d4305c5673ccf2eef2c325c04c98d8020a29
Simon Piscitelli <simonpiscitelli@gmail.com>
2019-11-13 08:55:37 +0000

    Delete scripts

--- docker/dev.sh
+++
@@ -1,6 +0,0 @@
-clear
-docker-compose down
-docker-compose up -d mysql
-sleep 10
-docker-compose up -d
-docker-compose logs -f server
--- docker/test.sh
+++
@@ -1,2 +0,0 @@
-docker-compose down
-docker-compose -f docker-compose.test.yml up --abort-on-container-exit  || exit 1
