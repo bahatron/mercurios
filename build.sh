6560c3b42d5ba17922a380af0f6c524264e41ac6
Simon Piscitelli <simonpiscitelli@gmail.com>
2020-01-24 23:54:11 +0000

    Refactor config test
    Update
    
    
    Try
    
    
    Try again

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 docker-compose -f docker-compose.build.yml build || exit 1
 ./test.sh || exit 1
 
-if [ [$TRAVIS_BRANCH = master] ];
+if [ [$MASTER = true] ];
 then
     docker login --username $DOCKER_USERNAME  --password $DOCKER_PASSWORD
     docker push bahatron/mercurios_http
