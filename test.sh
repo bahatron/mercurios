2c32d1acaeab54c07e8db9bf45c4b81acdaad8fc
Simon Piscitelli <simonpiscitelli@gmail.com>
2020-01-26 10:53:43 +0000

    Update test script

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 docker-compose -f docker-compose.test.yml down
 docker-compose -f docker-compose.test.yml up -d mysql
-sleep 15
-docker-compose -f docker-compose.test.yml up --abort-on-container-exit tests  || exit 1
+sleep 20
+docker-compose -f docker-compose.test.yml up --abort-on-container-exit  || exit 1
