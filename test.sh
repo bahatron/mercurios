57330010bb3883efdcfbc7d34d3f80b14b4dce67
Simon Piscitelli <simonpiscitelli@gmail.com>
2020-03-22 23:00:40 +0000

    Run benchmarks on CI

--- test.sh
+++ test.sh
@@ -3,4 +3,5 @@ docker-compose -f docker-compose.test.yml up -d mysql
 echo "waiting for mysql db to start..."
 sleep 30
 docker-compose -f docker-compose.test.yml up -d
-docker-compose -f docker-compose.test.yml exec server npm run test
+docker-compose -f docker-compose.test.yml exec server npm run test || exit 1
+docker-compose -f docker-compose.test.yml exec server npm run test:benchmark
