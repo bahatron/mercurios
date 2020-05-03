acde47dd5e70ec4c326cf1e4480c4f9e13e10cad
Simon Piscitelli <simonpiscitelli@gmail.com>
2020-05-03 11:31:54 +0100

    Update scripts

--- ci.sh
+++ ci.sh
@@ -2,17 +2,22 @@
 
 set -e
 
-docker-compose build
-./test.sh
+# docker-compose build
+# ./test.sh
 
-if [ ${_branch:-local} = master ];
-then
-    echo "master branch, pushing to repo"
-    docker login --username $DOCKER_USERNAME  --password $DOCKER_PASSWORD
-    docker tag mercurios_http bahatron/mercurios_http:${_tag:-latest}
+if [ ${_branch:-local} = master ]; then
+    echo "master branch, pushing to tag ${_tag}"
+    # docker login --username $DOCKER_USERNAME  --password $DOCKER_PASSWORD
+    # docker tag mercurios_http bahatron/mercurios_http:${_tag:-latest}
 
-    docker push bahatron/mercurios_http:${_tag:-latest}
+    # docker push bahatron/mercurios_http:${_tag:-latest}
+elif [ ${_branch:-local} != local ]; then
+    echo "pushing to tag ${_branch}"
+    # docker login --username $DOCKER_USERNAME  --password $DOCKER_PASSWORD
+    # docker tag mercurios_http bahatron/mercurios_http:${_branch}
+
+    # docker push bahatron/mercurios_http:${_branch}
 else
-    echo "no master, skipping push"
+    echo "skipping push"
 fi
 
