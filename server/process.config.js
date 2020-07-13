b09ded00c3dfa1921ae50cf5fbfef1dc032c9a62
Simon Piscitelli <simonpiscitelli@gmail.com>
2020-07-13 17:54:27 +0100

    Remove pm2 from production

--- server/process.config.js
+++ server/process.config.js
@@ -3,14 +3,8 @@ module.exports = {
         {
             script: "dist/bin/http_server.js",
             name: "mercurios",
-            exec_mode: "cluster",
-            restart_delay: "1000",
-            instances: process.env.MERCURIOS_WORKERS || 0,
-            watch:
-                process.env.MERCURIOS_ENV === "production" ? false : ["dist"],
-            autorestart: process.env.MERCURIOS_ENV === "production",
-            out_file: "/dev/null",
-            error_file: "/dev/null",
+            watch: true,
+            autorestart: false,
         },
     ],
 };
