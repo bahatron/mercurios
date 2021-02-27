121a6a65c15c9a89a32e885f5601b3d919f42a35
Simon Piscitelli <simonpiscitelli@gmail.com>
2021-02-27 20:59:46 +0000

    Fix pipeline tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,7 +2,9 @@
 set -x
 export GITROOT=$(git rev-parse --show-toplevel)
 
-docker-compose -f ${GITROOT}/docker-compose.test.yml up -d
+docker-compose -f ${GITROOT}/docker-compose.test.yml up -d mysql nats
+docker-compose -f ${GITROOT}/docker-compose.test.yml run -T mercurios-server \
+    sh -c "wait-for-it mysql:3306 -- npm start"
 
 docker-compose -f ${GITROOT}/docker-compose.test.yml run -T mercurios-server \
     sh -c "wait-for-it mercurios-server:4254 -t 30 -- npm run test" || exit 1
