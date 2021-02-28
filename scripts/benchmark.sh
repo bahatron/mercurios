d135040e9e3638e688768ba873f0a2fd9d95cd46
Simon Piscitelli <simonpiscitelli@gmail.com>
2021-02-28 18:45:18 +0000

    Remove benchmark script

--- scripts/benchmark.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -x
-export GITROOT=$(git rev-parse --show-toplevel)
-
-docker-compose -f ${GITROOT}/docker-compose.test.yml up -d
-
-docker exec mercurios-server sh -c \
-    "wait-for-it localhost:4254 -t 60 -s -- npm run benchmark -- --write --read --ping"
