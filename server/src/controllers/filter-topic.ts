d9ea739fd9cb31abd5c321d07502871863b340bb
Simon Piscitelli <simonpiscitelli@gmail.com>
2021-04-21 14:19:44 +0100

    Sort filtered stream by seq

--- server/src/controllers/filter-topic.ts
+++ server/src/controllers/filter-topic.ts
@@ -1,3 +1,4 @@
+import { sortBy } from "lodash";
 import { MercuriosEvent } from "../models/event/event";
 import { EventFilters } from "../models/store/drivers/helpers";
 import { $store } from "../models/store/store";
@@ -8,7 +9,7 @@ export default async function filterTopic(
     topic: string,
     query: EventFilters = {}
 ): Promise<MercuriosEvent[]> {
-    let result = await $store.filter(topic, query);
+    let result = sortBy(await $store.filter(topic, query), ["seq"]);
 
     $logger.debug(`topic filtered`, { topic, query, result });
 
