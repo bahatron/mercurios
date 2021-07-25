332da73a89681c6ac6c471538204f5541d7400af
Simon Piscitelli <simonpiscitelli@gmail.com>
2021-07-25 18:58:54 +0100

    Update test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,7 +6,8 @@ export GITROOT=$(git rev-parse --show-toplevel)
 docker-compose -f ${GITROOT}/docker-compose.test.yml up -d
 
 # run tests for mysql driver
-docker exec mercurios-postgres-driver sh -c "wait-for-it localhost:4254 -t 30 -- npm run test"
-docker exec mercurios-mysql-driver sh -c "wait-for-it localhost:4254 -t 30 -- npm run test"
+docker exec mercurios-postgres-driver sh -c "wait-for-it localhost:4254 -t 60 -- npm run test"
+docker exec mercurios-mysql-driver sh -c "wait-for-it localhost:4254 -t 60 -- npm run test"
+docker exec mercurios-mongo-driver sh -c "wait-for-it localhost:4254 -t 60 -- npm run test"
 
 MERCURIOS_URL=http://mercurios-postgres-driver:4254 docker exec mercurios-client sh -c "npm run test"
